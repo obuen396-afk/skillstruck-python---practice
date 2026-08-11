eb5b319385f6bdbca9821abff3eb9d2eb122b37d
obuen396-afk <obuen396@evit.edu>
2026-08-11 13:35:10 -0700

    exported from Skill Struck

---
+++ Python1/Math.py
@@ -0,0 +1,25 @@
+c = "a" + "b"
+print(c)
+c = 1 + 2
+print(c)
+c = 1 - 2
+print(c)
+c = 1 * "b"
+print(c)
+c = "a" * 2
+print(c)
+c = 1 * 2
+print(c)
+c = 1 / 2
+print(c)
+
+caterpillars = 3
+leaves = 25
+print(caterpillars * leaves)
+
+a = int(input("Enter a number."))
+b = int(input("Enter another number."))
+print(a + b)
+
+a_number = int(input("Pick a number!"))
+print(str(a_number - 1) + "," + str(a_number + 1))
