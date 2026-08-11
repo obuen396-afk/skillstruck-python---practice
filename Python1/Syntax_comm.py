f08ce148a265464d19f7ad0ecc7eade3d1088237
obuen396-afk <obuen396@evit.edu>
2026-08-11 13:35:05 -0700

    exported from Skill Struck

---
+++ Python1/Syntax_comm.py
@@ -0,0 +1,27 @@
+# This is a comment
+print('Did you see my comment?')
+# this is another comment!
+
+# This next line WILL NOT run
+# print('THIS LINE DOESN'T RUN')
+# The next line will run like normal
+print('This line is not commented')
+
+simple = "less than 1"
+print(simple)
+# This should be used somewhere...
+
+print("mom says stuff")
+# For my learning, sometimes
+print("dad says stuff")
+# Also, for my learning, sometimes...
+print("Dad says other stuff")
+# Something about games sometimes.
+
+sound = "voice"
+# I can hum to music when I feel it
+color = "Green"
+# My favorite color
+flavor = "nothing"
+# I don't know
+
