9a2b9c193a7046ba7d04a19b854c164d6686b2f8
obuen396-afk <obuen396@evit.edu>
2026-08-11 13:35:10 -0700

    exported from Skill Struck

---
+++ Python1/Modulus.py
@@ -0,0 +1,14 @@
+Div = int(10 / 7)
+print(Div)
+Mod = 10 % 7
+print("Remainder: " + str(Mod))
+
+Mod = int(input("Select a number!"))
+print(Mod % 7)
+
+members = int(input("How many family members?"))
+oranges = int(input("How many oranges?"))
+print(oranges % members)
+
+Mod = int(input("Number - 10 or bigger?"))
+print(Mod % 100)
