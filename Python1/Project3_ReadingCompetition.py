154862881c808f80e4ce6ec413b80a9e6e266fe5
obuen396-afk <obuen396@evit.edu>
2026-08-11 13:35:15 -0700

    exported from Skill Struck

---
+++ Python1/Project3_ReadingCompetition.py
@@ -0,0 +1,8 @@
+student = int(input("How many pages should a single student need? ")) #You can play with this.
+classroom = student * 30
+print(classroom)
+school = classroom * 12
+print(school)
+goal = int(input("what is the goal number of pages in the school? ")) #You can play with this.
+pages_per_student = goal / 360
+print(pages_per_student)
