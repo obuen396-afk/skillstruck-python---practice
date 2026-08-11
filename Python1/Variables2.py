da6ae51c2792686a263df191a81bca11259a216d
obuen396-afk <obuen396@evit.edu>
2026-08-11 13:35:06 -0700

    exported from Skill Struck

---
+++ Python1/Variables2.py
@@ -0,0 +1,48 @@
+# "'text'" numbers True/False 
+example = 34
+example2 = "34"
+print(example)
+print(example2)
+
+example = True
+example2 = "True"
+print(example)
+print(example2)
+
+Von_String = "Strings"
+Von_Integer = 10
+Von_Boolean = True
+print(Von_String)
+print(Von_Integer)
+print(Von_Boolean)
+
+Boo1 = True
+Boo12 = False
+
+FourtyTeg = 42
+Hi_String = "Hello!"
+Boot = True
+print(FourtyTeg)
+print(Hi_String)
+print(Boot)
+
+age = input("What is your age?")
+print("You are " + age + " years old.")
+
+did_do = "Play games"
+wish_you_did = "More time to play games"
+meals = 6
+homework = 0
+Sleep = True
+Sleep_Easy = False
+
+is_scared_of_heights = False
+print(is_scared_of_heights)
+loves_dogs = True
+print(loves_dogs)
+has_brown_eyes = True
+print(has_brown_eyes)
+knows_html = False
+print(knows_html)
+ate_breakfast_today = True
+print(ate_breakfast_today)
