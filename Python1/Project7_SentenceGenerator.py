c639428c47f19a0c13351f7fbdd617487498868c
obuen396-afk <obuen396@evit.edu>
2026-08-11 13:35:26 -0700

    exported from Skill Struck

---
+++ Python1/Project7_SentenceGenerator.py
@@ -0,0 +1,3 @@
+nouns = ["Harveys", "dog", "cat", "shark", "eagle", "giraffe", "snake", "building", "chair", "thing"]
+for x in nouns :
+    print("While I was taking my " + x + "for a walk, another giant " + x + "came out of nowhere and chased me all the way home!")
