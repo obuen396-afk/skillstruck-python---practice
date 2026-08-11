be56feb65408477a523411ac125c3b2c8e9b6435
obuen396-afk <obuen396@evit.edu>
2026-08-11 13:35:13 -0700

    exported from Skill Struck

---
+++ Python1/Methodic_Strings1.py
@@ -0,0 +1,11 @@
+air = "softer than water."
+print(air.replace("softer", "Air is thinner"))
+print(air.lower())
+water = "thicker than air."
+print(water.upper())
+
+One = input("Enter a sentence with the number 1 in it.")
+print(One.replace("1", "one"))
+
+Uppe = input("Enter a message.")
+print(Uppe.upper())
