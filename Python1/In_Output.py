76a5a6aa52aa8c49c876cc0d43da9714acd2fd26
obuen396-afk <obuen396@evit.edu>
2026-08-11 13:35:04 -0700

    exported from Skill Struck

---
+++ Python1/In_Output.py
@@ -0,0 +1,47 @@
+first_name = input("What is your first name?")
+
+favorite_animal = input('What is your favorite animal?')
+print(favorite_animal)
+
+color = input('What is your favorite color?')
+print(color)
+
+color = input('What is your favorite color?')
+print("My favorite color is " + color)
+
+favorite_animal = input('What is your favorite animal?')
+print('You said your favorite animal is a ' + favorite_animal + "!")
+
+color = input('What is your favorite color?')
+print("Your favorite color is " + color)
+
+dessert = input("What is your favorite dessert?")
+print(dessert)
+
+name = input("What is your name?")
+print("Welcome to the website " + name)
+
+name = input("What is your name?")
+print("Hello, " + name + "!")
+
+flavor = input("What ice cream flavor do you want?")
+mix_in = input("What mix in do you want?")
+topping = input("What topping do you want?")
+print(flavor)
+print(mix_in)
+print(topping)
+
+name = input("What is your name?")
+compliment = input("What compliment makes you feel good?")
+print("Hello " + name + ", I would like to give you a compliment. " + compliment)
+
+name = input("What is your name?")
+music = input("What is you music experience?")
+print("Hello " + name + " welcome to the guitar class! Your music experience: " + music)
+
+V1 = input("What do you like to do?")
+V2 = input("What's your favorite movie?")
+V3 = input("When is the BEST time of day?")
+V4 = input("What pet peeves do you have?")
+V5 = input("What's your favorite videogame?")
+print(V5)
