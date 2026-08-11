1a7d4fe42e678b95b83bd3ac43fb6cda960a4f73
obuen396-afk <obuen396@evit.edu>
2026-08-11 13:35:17 -0700

    exported from Skill Struck

---
+++ Python1/Project4_Decipher.py
@@ -0,0 +1,30 @@
+string1 = "3 pets ot og eulc txen eht dnif oT"
+reverse = string1[len(string1)::-1]
+print(reverse)
+
+b = reverse[::2]
+print(b)
+
+Answer_quote = ""
+quote_length = len("I drive men mad For the love of me, Easily Beaten, Never free. What Am I??")
+print(quote_length)
+
+string2 = "Alexandra, her father often said to himself, was like her grandfather; which was his way of saying that she was intelligent. John Bergson's father had been a shipbuilder, a man of considerable force and of some fortune. Late in life he married a second time, a Stockholm woman of questionable character, much younger than he, who goaded him into every sort of extravagance. On the shipbuilder's part, this marriage was an infatuation, the despairing folly of a powerful man who cannot bear to grow old. In a few years his unprincipled wife warped the probity of a lifetime. He speculated, lost his own fortune and funds entrusted to him by poor seafaring men, and died disgraced, leaving his children nothing. But when all was said, he had come up from the sea himself, had built up a proud little business with no capital but his own skill and foresight, and had proved himself a man. In his daughter, John Bergson recognized the strength of will, and the simple direct way of thinking things out, that had characterized his father in his better days. He would much rather, of course, have seen this likeness in one of his sons, but it was not a question of choice. As he lay there day after day he had to accept the situation as it was, and to be thankful that there was one among his children to whom he could entrust the future of his family and the possibilities of his hard-won land."
+check = "gold" in string2
+print(check)
+
+string3 = """When Anne reached the school that morning . . . for the first time in her life she had traversed the Birch Path deaf and blind to its beauties . . . all was quiet and still. The preceding teacher had trained the children to be in their places at her arrival, and when Anne entered the schoolroom she was confronted by prim rows of 'shining morning faces' and bright, inquisitive eyes. She hung up her hat and faced her pupils, hoping that she did not look as frightened and foolish as she felt and that they would not perceive how she was trembling. She had sat up until nearly twelve the preceding night composing a speech she meant to make to her pupils upon opening the school. She had revised and improved it painstakingly, and then she had learned it off by heart. It was a very good speech and had some very fine ideas in it, especially about gold and earnest striving after knowledge. The only trouble was that she could not now remember a word of it.
+
+After what seemed to her a year... about ten seconds in reality... she said faintly, "Take your Testaments, please," and sank breathlessly into her chair under cover of the rustle and clatter of desk lids that followed. While the children read their verses Anne marshaled her shaky wits into order and looked over the array of little pilgrims to the Grownup Land.
+
+Most of them were, of course, quite well known to her. Her own classmates had passed out in the preceding year but the rest had all gone to school with her, excepting the primer class and ten newcomers to Avonlea. Anne secretly felt more interest in these ten than in those whose possibilities were already fairly well mapped out to her. To be sure, they might be just as commonplace as the rest; but on the other hand there might be a genius among them. It was a thrilling idea.
+
+Sitting by himself at a corner desk was Anthony Pye. He had a dark, sullen little face, and was staring at Anne with a hostile expression in his black eyes. Anne instantly made up her mind that she would win that boy's affection and discomfit the Pyes utterly.
+
+In the other corner another strange boy was sitting with Arty Sloane... a jolly looking little chap, with a snub nose, freckled face, and big, light blue eyes, fringed with whitish lashes... probably the Donnell boy."""
+check2 = "gold" in string3
+print(check2)
+
+
+
+print(Answer_quote)
