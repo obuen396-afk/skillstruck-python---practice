83f42d421e2f4bce063854f5f4a8d82f15df4efc
obuen396-afk <obuen396@evit.edu>
2026-08-11 13:35:22 -0700

    exported from Skill Struck

---
+++ Python1/Project6_TeamRoster.py
@@ -0,0 +1,14 @@
+roster = ["Joe", "Madeline", "Jeremy", "Ahmed", "Li", "Sarah", "Xander", "Jane", "Savannah", "Juan"]
+roster_number = [10, 32, 29, 19, 78, 90, 55, 72, 11, 64]
+print("Joe " + str(10))
+print("Madeline " + str(32))
+print("Jeremy " + str(29))
+print("Ahmed " + str(19))
+print("Li " + str(78))
+print("Sarah " + str(90))
+print("Xander " + str(55))
+print("Jane " + str(72))
+print("Savannah " + str(11))
+print("Juan " + str(64))
+string1 = "Welcome to the team! Your team number is {}."
+print(string1.format(roster_age))
