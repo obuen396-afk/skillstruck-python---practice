803f12498743288fad2e3cccc9dc189b7b0b3fab
obuen396-afk <obuen396@evit.edu>
2026-08-11 13:35:03 -0700

    exported from Skill Struck

---
+++ Python1/Variables.py
@@ -0,0 +1,36 @@
+my_name = "My name is Fred"
+number = 13
+print(my_name)
+age = 18
+print(age)
+print(number)
+
+how_much_wood_would_a_wood_chuck_chuck_if_a_wood_chuck_could_chuck_wood = ("lots")
+print(how_much_wood_would_a_wood_chuck_chuck_if_a_wood_chuck_could_chuck_wood)
+
+main_course = "Noodles"
+side = "Oranges"
+drink = "Milk"
+print(main_course)
+print(side)
+print(drink)
+
+basketballs = 40
+people = 6
+hamburgers = 50
+print(basketballs)
+print(people)
+print(hamburgers)
+
+hero1 = "First"
+hero2 = "Second"
+hero3 = "Third"
+print(hero1)
+print(hero2)
+print(hero3)
+what_goes_up_when_rain_comes_down = "air?"
+print(what_goes_up_when_rain_comes_down)
+what_is_full_of_holes_but_holds_water = "Soil"
+print(what_is_full_of_holes_but_holds_water)
+what_can_you_not_keep_until_you_give_it = "Kind?"
+print(what_can_you_not_keep_until_you_give_it)
