807ec33688eab08aee47df21ace9036ec4f66fa4
obuen396-afk <obuen396@evit.edu>
2026-08-11 13:35:21 -0700

    exported from Skill Struck

---
+++ Python1/Lists.py
@@ -0,0 +1,10 @@
+candy = ["Lollipop", "Sour Patch Kids", "Trolli Sour Crawlers", "Sweet-Tart Ropes"]
+pieces = [3, 50, 30, 10]
+print(candy)
+print(pieces)
+
+friends = ["mom", "dad", "blue"]
+print(friends)
+
+reasons = ['I want to make games', 'It\'s interesting', 'I want to know something better than scratch.']
+print(reasons)
