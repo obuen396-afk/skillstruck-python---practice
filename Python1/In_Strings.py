7ccda2cc2bc5626b5e4149f540796bc0a81cf33e
obuen396-afk <obuen396@evit.edu>
2026-08-11 13:35:16 -0700

    exported from Skill Struck

---
+++ Python1/In_Strings.py
@@ -0,0 +1,12 @@
+string = "I do see a d. I DON\'T see an M."
+check = "D" in string
+print(check)
+check_no = "i" not in string
+print(check_no)
+
+armadillo = input("Enter a sentence, and consider the word, \"armadillo\" ")
+print("armadillo" in armadillo)
+
+
+a = input("Enter a sentence. ")
+print("a" in a)
