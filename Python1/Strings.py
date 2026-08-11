e1f7279d6c786116171fe37847cbeafb1fd1fdf1
obuen396-afk <obuen396@evit.edu>
2026-08-11 13:35:12 -0700

    exported from Skill Struck

---
+++ Python1/Strings.py
@@ -0,0 +1,19 @@
+Multi_String = """Hello!
+There are multiple 
+lines here!
+And this is still
+the same variable!"""
+
+short_quote = "Hello!"
+long_quote = """Hi!
+Hillo!
+Hello!
+Helloooooooo!
+Oops..."""
+print(short_quote)
+print(long_quote)
+
+journal = """\033[1;34mDay one:\033[0m
+I played games a lot
+I also did EVIT."""
+print(journal)
