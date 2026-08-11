b6685f928e17248eff4cf19c7563fa5ee403eed0
obuen396-afk <obuen396@evit.edu>
2026-08-11 13:35:06 -0700

    exported from Skill Struck

---
+++ Python1/Numbers.py
@@ -0,0 +1,47 @@
+# int.float
+age = 24
+siblings = 3
+temperature = -50
+gpa = 4.0
+pizza_slices = 2.5
+dessert_slices = -1.75
+
+One = 1
+Point = 1.0
+print(One)
+print(Point)
+
+feet_in_meters = 3.28084
+print(feet_in_meters)
+
+hamburger = 20
+hamburger_float = 19.99
+candy = 25
+candy_float = 24.99
+movie_ticket = 26
+movie_ticket_float = 25.99
+video_game = 60
+video_game_float = 59.99
+
+rank_outdoors = 5
+rank_indoors = 10
+time_outdoors = 24.99
+time_indoors = 75.01
+print(rank_outdoors)
+print(rank_indoors)
+print(time_outdoors)
+print(time_indoors)
+
+teams = 2
+new = 4
+outside = 3
+travel = 2
+school = 2
+teamsF = 2.5
+newF = 4.1
+outsideF = 3.6
+travelF = 2.6
+schoolF = 2.3
+
+average_pets = 6.5
+print(average_pets)
